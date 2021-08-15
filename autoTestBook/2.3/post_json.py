fb9e05330e72d24b650acc2ee6c1eccfd21e1dd1
zhang-chao-zhi <66006636+zhang-chao-zhi@users.noreply.github.com>
2021-08-15 21:23:22 +0800

    Delete autoTestBook directory

--- autoTestBook/2.3/post_json.py
+++
@@ -1,9 +0,0 @@
-#-*-coding:utf-8-*-
-import requests,json
-
-url_json = 'http://httpbin.org/post'
-data_json = json.dumps({'stock_no':'600585','price':'52.12'})   #dumps：可以将python对象解码为json数据
-res = requests.post(url_json,data_json)
-print(res)
-print(res.text)
-print(res.content)
--- autoTestBook/2.3/post_multi_files.py
+++
@@ -1,6 +0,0 @@
-#-*-coding:utf-8-*-
-import requests,json
-url  = 'http://httpbin.org/post'
-multiple_files = [('images', ('test1.png', 'test1.png', 'rb'), 'impage/png'), ('images', ('test1.png', 'test2.png', 'rb'), 'impage/png')]
-response = requests.post(url, files=multiple_files)
-print(response.text)
